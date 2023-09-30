2b72a54dc7437783392c891db6a7cff66415cee9
Boonyarit Iamsa-ard <boonyarit.iamsaard@gmail.com>
2023-09-30 19:45:06 +0700

    build: add dependencies injection

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:news_app_with_clean_architecture/src/config/router/app_router.dart';
-import 'package:news_app_with_clean_architecture/src/config/theme/app_theme.dart';
-import 'package:news_app_with_clean_architecture/src/core/utils/constants.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'src/config/router/app_router.dart';
+import 'src/config/theme/app_theme.dart';
+import 'src/core/utils/constants/constants.dart';
+import 'src/domain/repositories/api_repository.dart';
+import 'src/locator.dart';
+import 'src/presentation/cubits/remote_articles/remote_articles_cubit.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await initializeDependencies();
 
   runApp(App());
 }
@@ -16,13 +23,20 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      debugShowCheckedModeBanner: false,
-      title: appTitle,
-      theme: AppTheme.light,
-      routerConfig: _appRouter.router,
-      routerDelegate: _appRouter.router.routerDelegate,
-      routeInformationParser: _appRouter.router.routeInformationParser,
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => RemoteArticlesCubit(
+            locator<ApiRepository>(),
+          )..getBreakingNews(),
+        )
+      ],
+      child: MaterialApp.router(
+        debugShowCheckedModeBanner: false,
+        title: appTitle,
+        theme: AppTheme.light,
+        routerConfig: _appRouter.router,
+      ),
     );
   }
 }
