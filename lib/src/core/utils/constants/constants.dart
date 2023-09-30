46f1de94826c849f9bc58b3a157895f3d357fe5a
Boonyarit Iamsa-ard <boonyarit.iamsaard@gmail.com>
2023-09-30 19:27:06 +0700

    build: update constants and add scroll controller extension

---
+++ lib/src/core/utils/constants/constants.dart
@@ -0,0 +1,6 @@
+const String appTitle = 'News App';
+
+const String baseUrl = 'https://newsapi.org/v2';
+const String defaultApiKey = 'a4463dd910384777a79f151468b18e8d';
+const String defaultCountry = 'th';
+const int defaultPageSize = 20;
