7da33044afd9ce742e446148f0d33ed665ddf575
Boonyarit Iamsa-ard <boonyarit.iamsaard@gmail.com>
2023-09-30 20:59:25 +0700

    build: update constants

--- lib/src/core/utils/constants/constants.dart
+++ lib/src/core/utils/constants/constants.dart
@@ -2,5 +2,5 @@ const String appTitle = 'News App';
 
 const String baseUrl = 'https://newsapi.org/v2';
 const String defaultApiKey = 'a4463dd910384777a79f151468b18e8d';
-const String defaultCountry = 'th';
+const String defaultCountry = 'us';
 const int defaultPageSize = 20;
