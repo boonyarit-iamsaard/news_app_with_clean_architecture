4739ac567e469e5347597a2538b1be51466462e6
Boonyarit Iamsa-ard <boonyarit.iamsaard@gmail.com>
2023-09-30 19:45:46 +0700

    build: update breaking news view

--- lib/src/presentation/views/breaking_news_view.dart
+++ lib/src/presentation/views/breaking_news_view.dart
@@ -1,29 +1,75 @@
 import 'package:flutter/material.dart';
-import 'package:news_app_with_clean_architecture/src/core/utils/constants.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:go_router/go_router.dart';
 
-class BreakingNewsView extends StatelessWidget {
+import '../../core/utils/extensions/scroll_controller_extension.dart';
+import '../cubits/remote_articles/remote_articles_cubit.dart';
+
+class BreakingNewsView extends HookWidget {
   const BreakingNewsView({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final remoteArticlesCubit = BlocProvider.of<RemoteArticlesCubit>(context);
+    final scrollController = useScrollController();
+
+    useEffect(() {
+      scrollController.onScrollEndsListener(() {
+        remoteArticlesCubit.getBreakingNews();
+      });
+
+      return scrollController.dispose;
+    }, const []);
+
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: const Text(
-          appTitle,
+          'Daily News',
           style: TextStyle(
             color: Colors.black,
           ),
         ),
         centerTitle: true,
-      ),
-      body: const Center(
-        child: Text(
-          'Breaking News',
-          style: TextStyle(
-            fontWeight: FontWeight.w500,
+        actions: [
+          GestureDetector(
+            onTap: () {
+              context.goNamed('saved-articles');
+            },
+            child: const Padding(
+              padding: EdgeInsets.only(right: 14),
+              child: Icon(
+                Icons.bookmark,
+                color: Colors.black,
+              ),
+            ),
           ),
-        ),
+        ],
+      ),
+      body: BlocBuilder<RemoteArticlesCubit, RemoteArticlesState>(
+        builder: (_, state) {
+          switch (state.runtimeType) {
+            case RemoteArticlesLoading:
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            case RemoteArticlesFailed:
+              return Center(
+                child: IconButton(
+                  icon: const Icon(Icons.refresh),
+                  onPressed: () {
+                    remoteArticlesCubit.getBreakingNews();
+                  },
+                ),
+              );
+            case RemoteArticlesSuccess:
+              return const Center(
+                child: Text('Success'),
+              );
+            default:
+              return const SizedBox();
+          }
+        },
       ),
     );
   }
