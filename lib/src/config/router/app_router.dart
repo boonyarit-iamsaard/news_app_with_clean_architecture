a6f10abed102e872397243ae425d5d7b0a651f47
Boonyarit Iamsa-ard <boonyarit.iamsaard@gmail.com>
2023-09-30 18:23:45 +0700

    build: update linter rules

--- lib/src/config/router/app_router.dart
+++ lib/src/config/router/app_router.dart
@@ -1,5 +1,6 @@
 import 'package:go_router/go_router.dart';
-import 'package:news_app_with_clean_architecture/src/presentation/views/breaking_news_view.dart';
+
+import '../../presentation/views/breaking_news_view.dart';
 
 class AppRouter {
   GoRouter get router => GoRouter(
