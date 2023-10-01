bb6f09817da9eed38c92f00af664494f3512b082
Boonyarit Iamsa-ard <boonyarit.iamsaard@gmail.com>
2023-10-01 11:34:54 +0700

    build: update app router

--- lib/src/config/router/app_router.dart
+++ lib/src/config/router/app_router.dart
@@ -1,6 +1,9 @@
 import 'package:go_router/go_router.dart';
 
+import '../../domain/models/article.dart';
+import '../../presentation/views/article_details_view.dart';
 import '../../presentation/views/breaking_news_view.dart';
+import '../../presentation/views/saved_articles_views.dart';
 
 class AppRouter {
   GoRouter get router => GoRouter(
@@ -9,6 +12,21 @@ class AppRouter {
             name: 'breaking-news',
             path: '/',
             builder: (context, state) => const BreakingNewsView(),
+          ),
+          GoRoute(
+            name: 'article-details',
+            path: '/article-details',
+            builder: (context, state) {
+              final article =
+                  (state.extra as Map<String, dynamic>)['article'] as Article;
+
+              return ArticleDetailsView(article: article);
+            },
+          ),
+          GoRoute(
+            name: 'saved-articles',
+            path: '/saved-articles',
+            builder: (context, state) => const SavedArticlesView(),
           )
         ],
       );
